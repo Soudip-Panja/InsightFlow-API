a125af03398b6569864cb88e97f17c0a25cebfaa
Soudip Panja <soudip03panja@gmail.com>
2026-04-26 17:18:21 +0530

    feat: cors and vercel file added

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ const { seedUser } = require("./seedData/usersSeeding");
 // seedUser();
 
 const express = require("express");
+const cors = require("cors");
 const { PORT } = require("./config/env");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const authRoutes = require("./controllers/auth.controller");
 
 app.use("/v1/auth", authRoutes);
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
