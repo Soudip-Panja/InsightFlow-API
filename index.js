dc09525b8eeb6ee3e6cbb3a567abf8fbe17cd374
Soudip Panja <soudip03panja@gmail.com>
2026-04-29 16:28:31 +0530

    fix: auth move to routes folder

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const authRoutes = require("./controllers/auth.controller");
+const authRoutes = require("./routes/auth.routes");
 const projectRoutes = require("./routes/project.routes");
 
 app.use("/v1/auth", authRoutes);
-app.use("/v1/projects", projectRoutes)
+app.use("/v1/projects", projectRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
